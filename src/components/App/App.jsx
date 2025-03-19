063fe8ded4162a2e5f2c35936acfbd9c7db2c5fc
Dmytro <d.salyakin@gmail.com>
2025-03-20 01:30:19 +0200

    add examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,14 @@ import UserForm from '../UserForm/UserForm';
 import css from './App.module.css';
 
 export default function App() {
+  const addUser = (newUser) => {
+    console.log('Adding new user:', newUser);
+  };
   return (
     <div className={css.container}>
       <h1>Forms with Formik</h1>
 
-      <UserForm />
+      <UserForm onAdd={addUser} />
     </div>
   );
 }
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,57 +1,102 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 import css from './UserForm.module.css';
 
-export default function UserForm() {
+const UserSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'Must be min 3 chars')
+    .required('This field is required'),
+  email: Yup.string().email('Invalid email').required('This field is required'),
+  role: Yup.string()
+    .oneOf(['guest', 'user', 'admin'])
+    .required('This field is required'),
+  notif: Yup.array()
+    .of(Yup.string().oneOf(['email', 'sms', 'push']))
+    .max(3),
+});
+
+export default function UserForm({ onAdd }) {
+  const handleSubmit = (values, actions) => {
+    // console.log('handleSubmit', values);
+    onAdd(values);
+    actions.resetForm();
+  };
+
   return (
-    <form className={css.form}>
-      <div className={css.group}>
-        <label className={css.label}>Username:</label>
-        <input className={css.input} type="text" name="username" />
-      </div>
-
-      <div className={css.group}>
-        <label className={css.label}>Email:</label>
-        <input className={css.input} type="email" name="email" />
-      </div>
-
-      <div className={css.group}>
-        <label className={css.label}>Role:</label>
-        <select className={css.input} name="role">
-          <option value="guest">Guest</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </select>
-      </div>
-
-      <fieldset className={css.group}>
-        <legend className={css.legend}>Notification Preferences</legend>
-        <label className={css.checkboxLabel}>
-          <input
-            className={css.checkboxInput}
-            type="checkbox"
-            name="email_notif"
+    <Formik
+      initialValues={{
+        username: '',
+        email: 'poly@mail.com',
+        role: 'user',
+        notif: [],
+      }}
+      onSubmit={handleSubmit}
+      validationSchema={UserSchema}
+    >
+      <Form className={css.form}>
+        <div className={css.group}>
+          <label className={css.label}>Username:</label>
+          {/* <input className={css.input} type="text" name="username" /> */}
+          <Field className={css.input} type="text" name="username" />
+          <ErrorMessage
+            className={css.error}
+            component="span"
+            name="username"
           />
-          Receive email notifications
-        </label>
-        <label className={css.checkboxLabel}>
-          <input
-            className={css.checkboxInput}
-            type="checkbox"
-            name="sms_notif"
-          />
-          Receive SMS notifications
-        </label>
-        <label className={css.checkboxLabel}>
-          <input
-            className={css.checkboxInput}
-            type="checkbox"
-            name="push_notif"
-          />
-          Receive push notifications
-        </label>
-      </fieldset>
-      <button className={css.button} type="submit">
-        Submit
-      </button>
-    </form>
+        </div>
+
+        <div className={css.group}>
+          <label className={css.label}>Email:</label>
+          {/* <input className={css.input} type="email" name="email" /> */}
+          <Field className={css.input} type="email" name="email" />
+          <ErrorMessage className={css.error} component="span" name="email" />
+        </div>
+
+        <div className={css.group}>
+          <label className={css.label}>Role:</label>
+          <Field as="select" className={css.input} name="role">
+            <option value="guest">Guest</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </Field>
+          <ErrorMessage className={css.error} component="span" name="role" />
+        </div>
+
+        <fieldset className={css.group}>
+          <legend className={css.legend}>Notification Preferences</legend>
+          <label className={css.checkboxLabel}>
+            <Field
+              className={css.checkboxInput}
+              type="checkbox"
+              name="notif"
+              value="email"
+            />
+            Receive email notifications
+          </label>
+          <label className={css.checkboxLabel}>
+            <Field
+              className={css.checkboxInput}
+              type="checkbox"
+              name="notif"
+              value="sms"
+            />
+            Receive SMS notifications
+          </label>
+          <label className={css.checkboxLabel}>
+            <Field
+              className={css.checkboxInput}
+              type="checkbox"
+              name="notif"
+              value="push"
+            />
+            Receive push notifications
+          </label>
+        </fieldset>
+        <button className={css.button} type="submit">
+          Submit
+        </button>
+      </Form>
+    </Formik>
   );
 }
