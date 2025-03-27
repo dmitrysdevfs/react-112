58e436ec55fe1bae89cb1a8712d60b61e35733c6
Dmytro <d.salyakin@gmail.com>
2025-03-28 00:52:11 +0200

    Install Redux libs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,9 @@
-import { Route, Routes } from 'react-router';
-import AppHeader from '../AppHeader/AppHeader';
-import css from './App.module.css';
-import HomePage from '../../pages/HomePage';
-import UserPage from '../../pages/UserPage';
-import NotFoundPage from '../../pages/NotFoundPage';
-import UserDitailsPage from '../../pages/UserDetailsPage';
-import UserPosts from '../UserPosts/UserPosts';
-import UserTodos from '../UserTodos/UserTodos';
-
 export default function App() {
   return (
-    <div className={css.container}>
-      <AppHeader />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<UserPage />} />
-        <Route path="/dashboard/:userId" element={<UserDitailsPage />}>
-          <Route path="posts" element={<UserPosts />} />
-          <Route path="todos" element={<UserTodos />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+    <div>
+      <h1 style={{ textDecoration: 'underline' }}>
+        State management with Redux
+      </h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import 'modern-normalize';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
